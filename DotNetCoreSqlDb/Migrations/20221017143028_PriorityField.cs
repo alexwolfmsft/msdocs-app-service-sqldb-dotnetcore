98806db8f04880519b72076eefb301fbf054f3aa
Alex Wolf <alexwolf@microsoft.com>
2022-10-17 10:31:26 -0400

    test

---
+++ DotNetCoreSqlDb/Migrations/20221017143028_PriorityField.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    public partial class PriorityField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "Todo",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Todo");
+        }
+    }
+}
