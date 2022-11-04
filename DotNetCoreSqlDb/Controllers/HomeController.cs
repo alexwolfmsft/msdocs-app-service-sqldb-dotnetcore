4dc55d1cc0ea8c4a14ec0521e2751d78d208c735
alexwolfmsft <93200798+alexwolfmsft@users.noreply.github.com>
2022-11-04 16:12:24 -0400

    changes

--- DotNetCoreSqlDb/Controllers/HomeController.cs
+++ DotNetCoreSqlDb/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace DotNetCoreSqlDb.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            Console.Log("hi");
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
